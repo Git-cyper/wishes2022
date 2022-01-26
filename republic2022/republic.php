c4e799d4e1016e0fb8ad3d0dae94f30782bcd234
Gagan <ry315971@gmail.com>
2022-01-26 07:31:26 +0530

    wishes2022

---
+++ republic2022/republic.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>!Happy Republic Day!</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="/image/republicday-image2.png" type="image/x-icon">
+</head>
+<body> 
+    <div class="container">
+    <div>
+    <h1><?php echo $_GET["text-name"]; ?></h1>
+    </div>
+    <div id="image1"><img src="/image/की तरफ से.png" alt="ki-taraf-se" width="200vw" height="auto"></div>
+        <div id="image2"><img src="/image/गणतंत्र दिवस.png" alt="gantantra-diwas" width="250vw" height="auto"></div>
+        <div id="image3"><img src="/image/republicday.png" alt="republicday" width="300vw" height="auto"></div>
+
+        <div id="quote">
+            <marquee id="quote1" behavior="alternate" direction="left">
+                <b>वतन हमारा मिसाल मोहब्बत की</b>
+            </marquee>
+            <marquee id="quote2" behavior="alternate" direction="right">
+                <b> तोड़ता है दीवार नफ़रत की,मेरी खुश नसीबी</b>
+            </marquee>
+            <marquee id="quote3" behavior="alternate" direction="left">
+                <b> मेरी खुश नसीबी,मिली ज़िंदगी इस चमन में </b>
+            </marquee>
+            <marquee id="quote4" behavior="alternate" direction="right">
+                <b> भुला ना सके कोई इसकी ख़ुश्बू सातों जनम में।</b>
+            </marquee>
+        </div>
+        <div id="text">
+            <p>!गणतंत्र दिवस की हार्दिक शुभकामनाये!</p>
+        </div>
+        <div id="sending-text">
+                <a href="whatsapp://send?text=*message*">Share On Whatsapp</a>
+            </div>
+    </div>
+</body>
+</html>
